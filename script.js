f7c8dc45955987f96c039e6ca86d8d7e8f08b6f7
“ashrhea1986” <ashleyrhea1986@yahoo.com>
2021-10-11 16:16:11 -0500

    Completed exercise and bonus

---
+++ script.js
@@ -0,0 +1,69 @@
+// 1
+for (i=1; i<=7; i++){
+    console.log(i);
+};
+
+// 2
+for (i=5; i<=25; i+=4){
+    console.log(i);
+};
+
+// 3a 
+const wizards = [
+    "Harry Potter",
+    "Hermonie Granger", 
+    "Ron Weasley"
+];
+
+// 3b
+for (mainCharacters of wizards){
+    console.log(mainCharacters);
+};
+
+// 4a 
+let harryPotterMovies = 0; 
+
+// 4b 
+while (harryPotterMovies < 8){
+    harryPotterMovies++;
+}
+// 4c
+console.log(harryPotterMovies);
+
+// Bonus 
+
+// 5a 
+const hogwartsHouses = [
+    "Griffindor",
+    "Hufflepuff",
+    "Ravenclaw",
+    "Slytherin"
+]
+
+// 5b
+for (category of hogwartsHouses){
+    for (char of hogwartsHouses[0]){
+        console.log(char);
+    }
+    for (char of hogwartsHouses[1]){
+        console.log(char);
+    }
+    for (char of hogwartsHouses[2]){
+        console.log(char);
+    }
+    for (char of hogwartsHouses[3]){
+        console.log(char);
+    }
+};
+
+// 6a 
+const quote = [
+    "Yer", 
+    "A",
+    "Wizard",
+    "Harry"
+];
+
+// 6b 
+let whatAreYou = quote.join(' ');
+console.log(whatAreYou);
